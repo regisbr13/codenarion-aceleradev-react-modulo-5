3c60bb014e2a5075c9e11fe61b468c96de3d51f6
regisbr13 <regislima1391@gmail.com>
2020-05-11 02:18:46 -0300

    Updates

---
+++ precache-manifest.3f2df334bb3fc14f568017bf5dbe72b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac2cb6a51179c8b9a608c8282f30fc21",
+    "url": "/codenarion-aceleradev-react-modulo-5/index.html"
+  },
+  {
+    "revision": "d2ebfdab28cae28185c4",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/css/main.eec970d9.chunk.css"
+  },
+  {
+    "revision": "5399423f90e25b88e69d",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/js/2.a18891e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/js/2.a18891e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d2ebfdab28cae28185c4",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/js/main.945f37ef.chunk.js"
+  },
+  {
+    "revision": "73cab1f5984c6d651235",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/js/runtime-main.324c98bd.js"
+  },
+  {
+    "revision": "82a7c06238e831cd8f70bf1182156c2e",
+    "url": "/codenarion-aceleradev-react-modulo-5/static/media/logo.82a7c062.svg"
+  }
+]);
